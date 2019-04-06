896b50c73b0379bbcf3aa5c649bd015db33b92c2
Tom Jowett <tomj_82@hotmail.co.uk>
2019-04-06 23:00:57 +0100

    fully working rps simulator!

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-let uchoice = '1';
-let cchoice = '3';
-
-console.log(uchoice)
-console.log(cchoice)
+let uwins = 0;
+let cwins = 0;
 
 function uwin() {
-    console.log("you won! hurray!");
+    console.log("you won! hurray!")
+    uwins++;
 }
 
 function draw() {
     console.log("you both chose the same, try again!");
 }
 
-console.log(uchoice === '1' && cchoice === '3')
-console.log(uchoice === '1' && cchoice === '2')
+function cwin() {
+    console.log('you lost!')
+    cwins++;
+}
+
 
 function playround () {
-    if (uchoice == '1' && cchoice == '3') {
+    let uchoice = prompt("Enter your choice", "Rock,Paper or Scissors");
+    let uuchoice = uchoice.toLowerCase();
+    let cchoice = Math.floor(Math.random()  * 3);
+    if (uuchoice == 'rock' && cchoice == '2') {
         uwin();
-    } else if (uchoice === '1' && cchoice === '2') {
+    } else if (uuchoice == 'rock' && cchoice == '1') {
         cwin();
-    } else {
+    } else if (uuchoice == 'paper' && cchoice == '0') {
+        uwin();
+    } else if (uuchoice == 'paper' && cchoice == '2') {
+        cwin();
+    } else if (uuchoice == 'scissors' && cchoice == '1') {
+        uwin();
+    } else if (uuchoice == 'scissors' && cchoice == '0') {
+        cwin();
+    }
+    else {
         draw();
     }
 }
 
-playround()
+
+while (uwins < 5 && cwins < 5) {
+    playround();
+    console.log(uwins);
+    console.log(cwins);
+}
+
+if (uwins = 5) {
+    console.log('you are the champ!!');
+}
+else {
+    console.log('you lose the series!');
+}
